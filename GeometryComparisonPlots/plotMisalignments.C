d87472e0b09353498033823fa1cb1cbafe814478
Matthias Schroeder <matthias.schroeder@desy.de>
2015-01-27 18:24:45 +0100

    plotting geometry comparison for different systematic mis-alignment scnearios

---
+++ GeometryComparisonPlots/plotMisalignments.C
@@ -0,0 +1,37 @@
+{
+  gROOT->ProcessLine(".x loadPlotter.C");
+
+  const TString path = "GT_vs_misalign1.Comparison_commonTracker.root";
+
+  const double drMin    = -100;
+  const double drMax    =  100;
+  const double dzMin    = -100;
+  const double dzMax    =  100;
+  const double dxyMin   = -100;
+  const double dxyMax   =  100;
+  const double rdphiMin =  -20;
+  const double rdphiMax =   20;
+
+  const double scale = 5.;
+
+  GeometryComparison gc("GT_vs_misalign1.Comparison_commonTracker.root","Misalign_DetUnits_100mu");
+  gc.draw( "dr:r",   scale*drMin, scale*drMax );
+  gc.draw( "dr:z",   scale*drMin, scale*drMax );
+  gc.draw( "dr:phi", scale*drMin, scale*drMax );
+  
+  gc.draw( "dz:r",   scale*dzMin, scale*dzMax );
+  gc.draw( "dz:z",   scale*dzMin, scale*dzMax );
+  gc.draw( "dz:phi", scale*dzMin, scale*dzMax );
+  
+  gc.draw( "r*dphi:r",   scale*rdphiMin, scale*rdphiMax );
+  gc.draw( "r*dphi:z",   scale*rdphiMin, scale*rdphiMax );
+  gc.draw( "r*dphi:phi", scale*rdphiMin, scale*rdphiMax );
+  
+  gc.draw( "dx:r",   scale*dxyMin, scale*dxyMax );
+  gc.draw( "dx:z",   scale*dxyMin, scale*dxyMax );
+  gc.draw( "dx:phi", scale*dxyMin, scale*dxyMax );
+  
+  gc.draw( "dy:r",   scale*dxyMin, scale*dxyMax );
+  gc.draw( "dy:z",   scale*dxyMin, scale*dxyMax );
+  gc.draw( "dy:phi", scale*dxyMin, scale*dxyMax );
+}
